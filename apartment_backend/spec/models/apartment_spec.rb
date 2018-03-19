f864486a8f085681464e4c7c0017bbcea7ed9c9d
ndamicobarbour@gmail.com <learn@Mac-mini.localdomain>
2018-03-19 10:50:08 -0700

    installed rspec

--- apartment_backend/spec/models/apartment_spec.rb
+++ apartment_backend/spec/models/apartment_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Apartment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  it "can create an Apartment" do
+    apartment = Apartment.new
+    expect(apartment).to exist
+  end
+
 end
